6082eddb01edc9e6fc62c3001e3fe65be1c3ee0b
Ankit Kumar <ankit.kumar@samsung.com>
2024-09-30 21:17:33 +0530

    util: fix total fds to wait for
    The epoll file descriptor for a parent fd group waits for interrupt
    event on the fds in parent fd group as well as the fds in the child
    fd group. Add a new field num_descendant_fds to the structure
    spdk_fd_group that keeps track of all the fds in the child group.
    Use this to calculate the actual number of fds to wait for in
    spdk_fd_group_wait(), which is num_fds + num_descendant_fds.
    
    Add the missing unit tests for fd_group.
    
    Change-Id: Ib06de08e1083579540f9d0d0ee5fb8db23caad11
    Signed-off-by: Ankit Kumar <ankit.kumar@samsung.com>

---
+++ test/unit/lib/util/fd_group.c/fd_group.c
@@ -0,0 +1,88 @@
+/*   SPDX-License-Identifier: BSD-3-Clause
+ *   Copyright (C) 2024 Samsung Electronics Co., Ltd.
+ *   All rights reserved.
+ */
+
+#include "spdk/stdinc.h"
+#include "spdk_internal/cunit.h"
+#include "util/fd_group.c"
+
+static int
+fd_group_cb_fn(void *ctx)
+{
+	return 0;
+}
+
+static void
+test_fd_group_basic(void)
+{
+	struct spdk_fd_group *parent, *child;
+	int fd1, fd2, fd3;
+	int rc;
+	int cb_arg;
+
+	fd1 = epoll_create1(0);
+	fd2 = epoll_create1(0);
+	fd3 = epoll_create1(0);
+
+	rc = spdk_fd_group_create(&parent);
+	CU_ASSERT(rc == 0);
+
+	rc = spdk_fd_group_create(&child);
+	CU_ASSERT(rc == 0);
+
+	rc = SPDK_FD_GROUP_ADD(parent, fd1, fd_group_cb_fn, &cb_arg);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(parent->num_fds == 1);
+
+	rc = SPDK_FD_GROUP_ADD(child, fd2, fd_group_cb_fn, &cb_arg);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(child->num_fds == 1);
+
+	/* Nest child fd group to a parent fd group and verify it. */
+	rc = spdk_fd_group_nest(parent, child);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(parent->num_descendant_fds == 1);
+	CU_ASSERT(child->parent == parent);
+
+	/* Add a new fd to child fd group and verify number of descendant fds in parent. */
+	rc = SPDK_FD_GROUP_ADD(child, fd3, fd_group_cb_fn, &cb_arg);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(child->num_fds == 2);
+	CU_ASSERT(parent->num_descendant_fds == 2);
+
+	/* Unnest child fd group from a parent fd group and verify it. */
+	rc = spdk_fd_group_unnest(parent, child);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(parent->num_descendant_fds == 0);
+	CU_ASSERT(child->parent == NULL);
+
+	spdk_fd_group_remove(child, fd2);
+	spdk_fd_group_remove(child, fd3);
+	CU_ASSERT(child->num_fds == 0);
+
+	spdk_fd_group_remove(parent, fd1);
+	CU_ASSERT(parent->num_fds == 0);
+
+	spdk_fd_group_destroy(child);
+	spdk_fd_group_destroy(parent);
+}
+
+int
+main(int argc, char **argv)
+{
+	CU_pSuite	suite = NULL;
+	unsigned int	num_failures;
+
+	CU_initialize_registry();
+
+	suite = CU_add_suite("fd_group", NULL, NULL);
+
+	CU_ADD_TEST(suite, test_fd_group_basic);
+
+	num_failures = spdk_ut_run_tests(argc, argv, NULL);
+
+	CU_cleanup_registry();
+
+	return num_failures;
+}
